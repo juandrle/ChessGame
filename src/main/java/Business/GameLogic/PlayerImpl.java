a773bc9941f6cfe39c32406a8562328ae39a9f1b
julianandrle <julian.andrle@syntax-institut.de>
2023-06-07 14:57:58 +0200

    Playerinterface written

--- src/main/java/Business/GameLogic/PlayerImpl.java
+++ src/main/java/Business/GameLogic/PlayerImpl.java
@@ -1,4 +1,43 @@
 package Business.GameLogic;
 
+import Business.Gamepiece.Gamepiece;
+
+import java.util.List;
+
 public class PlayerImpl implements Player{
+    String name;
+    List<Gamepiece> ownGamepieces;
+    @Override
+    public void moveGamepiece(Gamepiece gamepiece) {
+    }
+
+    @Override
+    public Gamepiece chooseGamepiece() {
+        return null;
+    }
+
+    @Override
+    public void useItem() {
+
+    }
+
+    @Override
+    public void setName(String name) {
+
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public void removeGamepiece(Gamepiece gamepiece) {
+
+    }
+
+    @Override
+    public Gamepiece getGamepiece(Gamepiece gamepiece) {
+        return null;
+    }
 }
