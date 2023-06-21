25f6ea67db5cc485c1d21ddc85c8db0fa3280f3e
julianandrle <julian.andrle@syntax-institut.de>
2023-06-22 01:13:35 +0200

    merged

--- src/main/java/Business/Competition/CompetitionImpl.java
+++ src/main/java/Business/Competition/CompetitionImpl.java
@@ -4,90 +4,93 @@ import Business.Gamepiece.Gamepiece;
 import javafx.beans.property.SimpleBooleanProperty;
 import javafx.beans.property.SimpleIntegerProperty;
 
-public class CompetitionImpl implements Competition{
-	private int time;
-	private int helpCountCorrectAnswer;
-	private SimpleIntegerProperty countCorrectAnswer;
-	private SimpleIntegerProperty actTime;
-	private int helpActTime;
+public class CompetitionImpl implements Competition {
+    private int time;
+    private int helpCountCorrectAnswer;
+    private SimpleIntegerProperty countCorrectAnswer;
+    private SimpleIntegerProperty actTime;
+    private int helpActTime;
 
-	private SimpleBooleanProperty gameEnds;
-	
-
-	public CompetitionImpl(int time) {
-		super();
-		this.time = time;
-		this.helpActTime = time;
-		this.helpCountCorrectAnswer = 0;
-		this.actTime = new SimpleIntegerProperty();
-		this.countCorrectAnswer = new SimpleIntegerProperty();
-		gameEnds = new SimpleBooleanProperty(false);
-	}
+    private SimpleBooleanProperty gameEnds;
 
 
-	@Override
-	public void setPoints(Gamepiece gamepiece) {
-		if (helpActTime == 0){
-			gamepiece.setPoints(helpCountCorrectAnswer);
-		}
-	}
+    public CompetitionImpl(int time) {
+        super();
+        this.time = time;
+        this.helpActTime = time;
+        this.helpCountCorrectAnswer = 0;
+        this.actTime = new SimpleIntegerProperty();
+        this.countCorrectAnswer = new SimpleIntegerProperty();
+        gameEnds = new SimpleBooleanProperty(false);
+    }
 
-	public SimpleBooleanProperty gameEndsProperty() {
-		return gameEnds;
-	}
 
-	@Override
-	public Gamepiece whoWin(Gamepiece gamepiece1,Gamepiece gamepiece2) {
-		if(gamepiece1.getPoints() > gamepiece2.getPoints()){
-			return gamepiece1;
-		}else{
-			return gamepiece2;
-		}
-	}
-	
-	@Override
-	public void startTimer() {
-		Thread timerThread = new Thread(() -> {
-			try {
-				while (helpActTime != 0) {
-					Thread.sleep(1000);
-					helpActTime-=1;
-					actTime.set(helpActTime);
-				}
+    @Override
+    public void setPoints(Gamepiece gamepiece) {
+        if (helpActTime == 0) {
+            gamepiece.setPoints(helpCountCorrectAnswer);
+        }
+    }
 
-				if (helpActTime == 0){
-					this.gameEnds.set(true);
-				}
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-		});
-		timerThread.start();
-		
-	}
+    public SimpleBooleanProperty gameEndsProperty() {
+        return gameEnds;
+    }
 
-	public void setHelpActTime(int helpActTime) {
-		this.helpActTime = helpActTime;
-	}
+    @Override
+    public Gamepiece whoWin(Gamepiece gamepiece1, Gamepiece gamepiece2) {
+        if (gamepiece1.getPoints() == gamepiece2.getPoints()) {
+            return null;
+        } else if (gamepiece1.getPoints() > gamepiece2.getPoints()) {
+            return gamepiece1;
+        } else {
+            return gamepiece2;
+        }
+    }
 
-	public SimpleIntegerProperty getActTime() {
-		return actTime;
-	}
+    @Override
+    public void startTimer() {
+        Thread timerThread = new Thread(() -> {
+            try {
+                while (helpActTime != 0) {
+                    Thread.sleep(1000);
+                    helpActTime -= 1;
+                    actTime.set(helpActTime);
+                }
 
-	public void setActTime(SimpleIntegerProperty actTime) {
-		this.actTime = actTime;
-	}
-	public int getHelpCountCorrectAnswer() {
-		return helpCountCorrectAnswer;
-	}
+                if (helpActTime == 0) {
+                    this.gameEnds.set(true);
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+        timerThread.start();
 
-	public void setHelpCountCorrectAnswer(int helpCountCorrectAnswer) {
-		this.helpCountCorrectAnswer += helpCountCorrectAnswer;
-		countCorrectAnswer.set(this.helpCountCorrectAnswer);
-	}
+    }
 
-	public SimpleIntegerProperty getCountCorrectAnswer() {
-		return countCorrectAnswer;
-	}
-	
+    public void setHelpActTime(int helpActTime) {
+        this.helpActTime = helpActTime;
+    }
+
+    public SimpleIntegerProperty getActTime() {
+        return actTime;
+    }
+
+    public void setActTime(SimpleIntegerProperty actTime) {
+        this.actTime = actTime;
+    }
+
+    public int getHelpCountCorrectAnswer() {
+        return helpCountCorrectAnswer;
+    }
+
+    public void setHelpCountCorrectAnswer(int helpCountCorrectAnswer) {
+        this.helpCountCorrectAnswer += helpCountCorrectAnswer;
+        countCorrectAnswer.set(this.helpCountCorrectAnswer);
+    }
+
+    public SimpleIntegerProperty getCountCorrectAnswer() {
+        return countCorrectAnswer;
+    }
+
 }
